0916b98d0ee8b3583ec19c1ba36af55a7a420a47
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-06-27 11:43:29 -0700

    initial commit
    started with simple world and player

---
+++ Scripts/ui.gd
@@ -0,0 +1,18 @@
+class_name UI extends Control
+
+@onready var sprint_meter := $SprintMeter as TextureProgressBar
+@onready var sprint_timer := $SprintMeter/Timer as Timer
+var sprint_value :float = 100. :
+	set(val):
+		val = clamp(val, sprint_meter.min_value, sprint_meter.max_value)
+		if val == sprint_meter.value: return
+		sprint_meter.value = val
+		sprint_value = val
+		if val == 100: sprint_meter.visible = false
+		else: sprint_meter.visible = true
+		print(sprint_meter.value)
+
+
+
+func _on_timer_timeout() -> void:
+	sprint_value += 0.3
